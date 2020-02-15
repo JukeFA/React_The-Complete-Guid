8c0cdbd96b976fcba152122b2b40bd3f6a3a096b
JukeFA <45007798+JukeFA@users.noreply.github.com>
2020-02-14 19:41:10 -0500

    List and Keys

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: 'oajsd', name: 'Max', age: 28 },
+      { id: 'asdfa', name: 'Manu', age: 29 },
+      { id: 'agasd', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
@@ -24,7 +24,8 @@ class App extends Component {
   }
 
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons
+    // const persons = this.state.persons.slice()
+    const persons = [...this.state.persons]
     persons.splice(personIndex, 1)
     this.setState({persons: persons})
   }
@@ -52,7 +53,8 @@ class App extends Component {
             return <Person 
             click={() => this.deletePersonHandler(index)}
             name={person.name}
-            age={person.age} />
+            age={person.age}
+            key={person.id} />
           })}
         </div>
       )
